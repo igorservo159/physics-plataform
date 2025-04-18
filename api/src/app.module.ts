2939a774714c27af57f857fa77e3124e36f78105
igorservo159 <igorservo159@gmail.com>
2025-04-18 00:38:18 -0300

    refactor: remove Mailer feature and replace dotenv with ConfigModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,37 +9,19 @@ import { GoogleSheetsController } from './google-sheets/google-sheets.controller
 import { MepModule } from './mep/mep.module';
 import { FirebaseAuthStrategy } from './auth/firebase.strategy';
 import { HelloModule } from './hello/hello.module';
-import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? undefined : '.env.dev',
+    }),
     UsersModule,
     AuthModule,
     MepModule,
     HelloModule,
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.sendgrid.net',
-        port: 465, // 587 for unencrypted/TLS connections, 465 for SSL connections
-        secure: true, // false for TLS, true for SSL
-        auth: {
-          user: 'apikey',
-          pass: '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',
-        },
-      },
-    }),
-    /*TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'FTS_teste',
-      entities: [Mep],
-      synchronize: false,
-    }),*/
   ],
   controllers: [AppController, GoogleSheetsController],
   providers: [AppService, GoogleSheetsService, FirebaseAuthStrategy],
 })
-export class AppModule {}
+export class AppModule { }
