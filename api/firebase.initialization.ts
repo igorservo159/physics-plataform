d7b76806797c42429c7bfd1e06e01486229f5bc9
igorservo159 <igorservo159@gmail.com>
2025-04-18 07:39:30 -0300

    refactor: use JSON parse to turn into object

--- api/firebase.initialization.ts
+++ api/firebase.initialization.ts
@@ -1,18 +1,16 @@
-// firebase.initialization.ts
-
-let firebaseConfigPath;
-
-// Determinando o caminho do arquivo de credenciais com base no ambiente
-if (process.env.NODE_ENV === 'production') {
-  firebaseConfigPath = 'config/prod/service-account.prod.json';
-} else {
-  firebaseConfigPath = 'config/dev/service-account.dev.json';
-}
-
 import * as firebase from 'firebase-admin';
+import * as fs from 'fs';
+
+// Determina o caminho do arquivo
+const firebaseConfigPath = process.env.NODE_ENV === 'production'
+  ? 'config/prod/service-account.prod.json'
+  : 'config/dev/service-account.dev.json';
+
+// Lê e transforma em objeto
+const serviceAccount = JSON.parse(fs.readFileSync(firebaseConfigPath, 'utf8'));
 
 const firebaseAdminApp = firebase.initializeApp({
-  credential: firebase.credential.cert(firebaseConfigPath),
+  credential: firebase.credential.cert(serviceAccount),
 });
 
 export default firebaseAdminApp;
