7b5ffb6161740172f43047b8c35d06b205bbc232
igorservo159 <igorservo159@gmail.com>
2025-04-18 10:07:31 -0300

    revert: remove debug env

--- api/firebase.initialization.ts
+++ api/firebase.initialization.ts
@@ -1,16 +1,10 @@
 import * as firebase from 'firebase-admin';
 import * as fs from 'fs';
-import * as path from 'path';
-
-// Mostra o valor atual de NODE_ENV
-console.log('NODE_ENV:', process.env.NODE_ENV);
 
 // Determina o caminho do arquivo
 const firebaseConfigPath = process.env.NODE_ENV === 'production'
   ? 'config/prod/service-account.prod.json'
   : 'config/dev/service-account.dev.json';
-
-console.log('firebaseConfigPath:', firebaseConfigPath);
 
 // Verifica se o arquivo existe
 if (!fs.existsSync(firebaseConfigPath)) {
@@ -20,12 +14,8 @@ if (!fs.existsSync(firebaseConfigPath)) {
 
 // Lê e transforma em objeto
 const raw = fs.readFileSync(firebaseConfigPath, 'utf8');
-console.log('Conteúdo bruto do arquivo JSON:', raw);
 
 const serviceAccount = JSON.parse(raw);
-
-// Verifica se project_id está presente
-console.log('project_id:', serviceAccount.project_id);
 
 // Inicializa o Firebase Admin
 const firebaseAdminApp = firebase.initializeApp({
